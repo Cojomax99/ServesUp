be9cf9e8f1c2c8a034f1e844cb4c481f73560352
Cojomax99 <cojomax99@gmail.com>
2013-12-13 14:55:27 -0500

    stupid auto commit still not working

--- net/cojo/servesup/entities/SUEntityRegistry.java
+++ net/cojo/servesup/entities/SUEntityRegistry.java
@@ -1,10 +1,25 @@
 package net.cojo.servesup.entities;
 
+import net.cojo.servesup.ModInfo;
+import net.cojo.servesup.ServesUp;
+import cpw.mods.fml.common.registry.EntityRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+
+/**
+ * Class for registering entities
+ * @author Cojo
+ *
+ */
 public class SUEntityRegistry {
+
+	/** ID for the entity */
+	private static int entityId = 0;
 	
-	public EntityVolleyball volleyball;
-	
+	/**
+	 * Init the volleyball entity
+	 */
 	public static void init() {
-		
+		EntityRegistry.registerModEntity(EntityVolleyball.class, "Volleyball", entityId++, ServesUp.instance, 64, 3, true);
+		LanguageRegistry.instance().addStringLocalization(String.format("entity.%s." + "Volleyball" + ".name", ModInfo.MODID), "en_US", "Volleyball");
 	}
 }
