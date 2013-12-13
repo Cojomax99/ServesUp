c7cf48193d324d7dfb457fa34540531031f1c3d9
Cojomax99 <cojomax99@gmail.com>
2013-12-13 16:54:31 -0500

    wot entity

--- src/packages/net/cojo/servesup/entities/render/RenderVolleyball.java
+++ src/packages/net/cojo/servesup/entities/render/RenderVolleyball.java
@@ -1,6 +1,7 @@
 package net.cojo.servesup.entities.render;
 
 import net.cojo.servesup.ModUtil;
+import net.cojo.servesup.entities.EntityVolleyball;
 import net.cojo.servesup.entities.model.ModelVolleyball;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.entity.Entity;
@@ -18,7 +19,9 @@ public class RenderVolleyball extends Render {
 	@Override
 	public void doRender(Entity entity, double d0, double d1, double d2,
 			float f, float f1) {
-
+		EntityVolleyball vball = (EntityVolleyball)entity;
+		this.bindEntityTexture(vball);
+		this.model.render(vball, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
 	}
 
 	@Override
