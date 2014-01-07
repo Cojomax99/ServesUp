5b59ccde5850db640b2bd897d626f474b4281c3e
Cojomax99 <cojomax99@gmail.com>
2014-01-07 17:57:11 -0500

    Added CourtData object for storing data

--- net/cojo/servesup/court/CourtBuilder.java
+++ net/cojo/servesup/court/CourtBuilder.java
@@ -7,6 +7,7 @@ import net.minecraft.world.World;
 
 public class CourtBuilder {
 
+	/** Number of blocks long and wide a side of a court is, excluding border */
 	private static final int INNER_SIZE = 11;
 
 	/**
@@ -18,24 +19,49 @@ public class CourtBuilder {
 	 * @param direction Cardinal direction to build the court in
 	 */
 	public static void buildCourt(World world, int x, int y, int z, int direction) {
-		buildBorder(world, x, y, z, direction);
+		CourtData data = buildBorder(world, x, y, z, direction);
+		fillCourt(world, data);
+	}
+	
+	/**
+	 * Fill in the court based on the data returned by buildBorder
+	 * @param world World instance
+	 * @param data Data of the court
+	 */
+	private static void fillCourt(World world, CourtData data) {
+		for (int i = data.minX + 1; i < data.maxX; i++) {
+			placeCourt(world, i, data.y, data.minZ);
+			placeCourt(world, i, data.y, data.maxZ);
+		}
+		
+		for (int i = data.minZ + 1; i < data.maxZ; i++) {
+			placeCourt(world, data.minX, data.y, i);
+			placeCourt(world, data.maxZ, data.y, i);
+		}
 	}
 
-	private static void buildBorder(World world, int x, int y, int z, int direction) {
+	/**
+	 * Build the court's border, return data for the court
+	 * @param world World instance
+	 * @param x xCoord of the court block
+	 * @param y yCoord of the court block
+	 * @param z zCoord of the court block
+	 * @param direction Determines court orientation
+	 * @return a CourtData object containing the minx, maxX, y, minZ, and maxZ values of the court
+	 */
+	private static CourtData buildBorder(World world, int x, int y, int z, int direction) {
 		int minX=0, minZ=0, maxX=0, maxZ=0;
 
-		minX = x - INNER_SIZE - 1;
-		maxX = x + INNER_SIZE + 1;
-		minZ = z - 1;
-		maxZ = z - 1 - INNER_SIZE - 1;
+		// Default direction to 0 if it is not in [0,3] inclusive
+		direction = direction < 0 || direction > 3 ? 0 : direction;
 
+		// Determine the min/max vals based on direction
 		switch (direction) {
 		case 0:
 			minX = x - INNER_SIZE - 1;
 			maxX = x + INNER_SIZE + 1;
 			minZ = z - 1 - INNER_SIZE - 1;
 			maxZ = z - 1;
-
 			break;
 		case 1:
 			minX = x - INNER_SIZE - 1;
@@ -48,7 +74,6 @@ public class CourtBuilder {
 			maxX = x - 1;
 			minZ = z - INNER_SIZE - 1;
 			maxZ = z + INNER_SIZE + 1;
-
 			break;
 		case 3:
 			minX = x + 1;
@@ -58,47 +83,35 @@ public class CourtBuilder {
 			break;
 		}
 
-		// To prevent edge cases in the X direction
+		// Place x-length border
 		for (int i = minX; i <= maxX; i++) {
 			placeBorder(world, i, y, minZ);
 			placeBorder(world, i, y, maxZ);
 		}
 
-		// To prevent edge cases in the Z direction
+		// Place z-length border
 		for (int i = minZ; i <= maxZ; i++) {
 			placeBorder(world, minX, y, i);
 			placeBorder(world, maxX, y, i);
 		}
+		
+		return new CourtData(minX, maxX, y, minZ, maxZ);
 	}
 
-	public static void placeBorder(World world, int x, int y, int z) {
-		place(world, x, y, z, Block.planks, 4);
-	}	
-
 	/**
-	 * Rotates a block to a new position given an angle 
-	 * @param coords ChunkCoordinates of the block to move
-	 * @param direction Angle to rotate, clamped to closest 90 degree value
-	 * @param start Minimum x,y,z block of structure
-	 * @param size Size of the structure
-	 * @return The new location of the block that used to be located at coords
+	 * Lazy method that just calls place, but for whatever block should be used as the border
 	 */
-	public static ChunkCoordinates rotateNew(ChunkCoordinates coords, int direction, Vec3 start, Vec3 size) {
-
-		System.out.println("direction: " + direction);
-
-		//center is not offsetted
-		//coords should be offset 0.5 before rotate math, guarantees no strange offset issues, flooring cleans it up afterwards perfectly
-
-		double rotation = (direction * Math.PI/2D);
-		double centerX = start.xCoord+(size.xCoord/2D);
-		double centerZ = start.zCoord+(size.zCoord/2D);
-		double vecX = (coords.posX+0.5D) - centerX;
-		double vecZ = (coords.posZ+0.5D) - centerZ;
-
-		Vec3 vec = Vec3.createVectorHelper(vecX, 0, vecZ);
-		vec.rotateAroundY((float)rotation);
-		return new ChunkCoordinates((int)Math.floor(start.xCoord+vec.xCoord), coords.posY, (int)Math.floor(start.zCoord+vec.zCoord));
+	public static void placeBorder(World world, int x, int y, int z) {
+		//TODO: configurize border block here
+		place(world, x, y, z, Block.planks, 4);
+	}
+	
+	/**
+	 * Lazy method that just calls place, but for whatever block should be used as the court
+	 */
+	public static void placeCourt(World world, int x, int y, int z) {
+		//TODO: configurize court block here
+		place(world, x, y, z, Block.sand);
 	}
 
 	public static void place(World world, int x, int y, int z, Block block) {
---
+++ net/cojo/servesup/court/CourtData.java
@@ -0,0 +1,33 @@
+package net.cojo.servesup.court;
+
+/**
+ * Simple class to hold the min and max coordinate data points
+ * @author Cojo
+ *
+ */
+public class CourtData {
+	
+	/** Smallest x coord on the court */
+	public int minX;
+	
+	/** Largest x coord on the court */
+	public int maxX;
+	
+	/** y-level of the court */
+	public int y;
+	
+	/** Smallest z coord on the court */
+	public int minZ;
+	
+	/** Largest z coord on the court */
+	public int maxZ;
+	
+	public CourtData(int minX, int maxX, int y, int minZ, int maxZ) {
+		this.minX = minX;
+		this.maxZ = maxZ;
+		this.y = y;
+		this.minZ = minZ;
+		this.maxZ = maxZ;
+	}
+	
+}
